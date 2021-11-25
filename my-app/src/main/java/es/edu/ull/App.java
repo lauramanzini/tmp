0344f6d6874d182351686ac068ffa057046ffd5a
lauramanzini <232566@studenti.unimore.it>
2021-11-25 11:05:13 +0000

    Initial commit

---
+++ my-app/src/main/java/es/edu/ull/App.java
@@ -0,0 +1,15 @@
+package es.edu.ull;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+         System.out.println( "Esta es la primera prueba" );
+    }
+}
+
